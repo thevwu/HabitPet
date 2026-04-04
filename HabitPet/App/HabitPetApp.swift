09bb2f07024ae9022d9c3b9195a97a876a0dd94d
m1 <m1@Mac-1775293429620.local>
2026-04-04 23:48:25 +0000

    Add TCA and app folder structure.

---
+++ HabitPet/App/HabitPetApp.swift
@@ -0,0 +1,7 @@
+//
+//  HabitPetApp.swift
+//  HabitPet
+//
+//  Created by m1 on 04/04/2026.
+//
+//  TODO
---
+++ HabitPet/Tests/HabitPetTests/HabitPetTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPetTests.swift
+//  HabitPetTests
+//
+//  Created by m1 on 04/04/2026.
+//
+
+import Testing
+@testable import HabitPet
+
+struct HabitPetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
